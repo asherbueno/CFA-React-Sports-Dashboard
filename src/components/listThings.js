47383644bb0352e64f8cb667e18ed93bd42f716c
ethankhoa <ethan.huynh@gmail.com>
2017-05-22 12:14:52 +1000

    updates listing component

--- src/components/listThings.js
+++ src/components/listThings.js
@@ -4,9 +4,9 @@ import { ListGroup, ListGroupItem } from 'reactstrap';
 const listThings = (props) => {
   return (
     <ListGroup>
-    {props.things.map((thing, i) => <ListGroupItem key={i}>{thing.name}</ListGroupItem>)}
-  </ListGroup>
+      {props.things.map((thing, i) => <ListGroupItem key={i}>{thing.name}</ListGroupItem>)}
+    </ListGroup>
   )
-}
+};
 
 export default listThings;
