4e2c57c8de5b252710ae58a492e4d8006d45274a
Asher Bueno <ash3rbueno@gmail.com>
2017-05-22 15:24:06 +1000

    added teams

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Teams from './components/Teams';
 
 class App extends Component {
   constructor(props) {
@@ -12,15 +11,26 @@ class App extends Component {
     }
   }
 
-  ComponentWillMount() {
-
+  componentWillMount() {
+    this.getTeams()
   };
 
+  getTeams() {
+    const URL = 'http://data.nba.net/data/10s/prod/v1/2016/teams.json'
+    fetch(URL)
+      .then((res) => {
+        return res.json();
+    })
+      .then((json) => {
+        this.setState({teams: json.league.standard});
+        console.log(json);
+      });
+  };
 
   render() {
     return (
       <div className="App">
-
+        <Teams teams={this.state.teams} />
       </div>
     );
   }
